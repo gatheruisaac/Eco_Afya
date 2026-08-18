dce38a245a9641f9ac57d668a2c46cfcc404bb9f
isaac gatheru <igatheru7@gmail.com>
2026-08-19 01:15:40 +0300

    feat: add application routing and pages

---
+++ src/pages/About.jsx
@@ -0,0 +1,5 @@
+function About() {
+  return <h1>About Eco Afya</h1>;
+}
+
+export default About;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home() {
+  return <h1>Welcome to Eco Afya</h1>;
+}
+
+export default Home;
